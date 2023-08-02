7bd23602641cd1cf4bc04730124507724b6be2e3
Dianelli-Leite <dianellicardoso@gmail.com>
2023-08-02 13:41:59 -0300

    adicionando um console.log

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conecando ao banco de dados")
 
 console.log("A dianelli fez esse commit")
+console.log("enviando para o github")
